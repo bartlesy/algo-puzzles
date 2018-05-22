f033a93a57ce662ffdd361292d21323c1c6266ab
Stanley Bartlett <stanleybartlett@northwesternmutual.com>
2018-05-22 12:42:56 -0400

    rotate linked list

---
+++ rotate_linked_list.py
@@ -0,0 +1,62 @@
+# https://leetcode.com/problems/rotate-list/description/
+
+class ListNode(object):
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+
+def build_linked_list(int_list):
+    nodes = [ListNode(x) for x in int_list]
+    for n1, n2 in zip(nodes, nodes[1:]):
+        n1.next = n2
+    return nodes[0]
+
+
+def traverse_list(head):
+    node = head
+    while node:
+        print(node.val)
+        node = node.next
+    return
+
+
+# naive solution
+def _rotate_right(head, k):
+    n_rots = 0
+    while n_rots < k:
+        node = head
+        last_node = None
+        while node.next:
+            last_node = node
+            node = node.next
+        node.next = head
+        head = node
+        last_node.next = None
+        n_rots += 1
+    return head
+
+# cheap hack to make naive solution workable
+# better method would be to just cut off the rotated list and rotate it all
+# at once, but effort
+def rotate_right(head, k):
+    n = 0
+    node = head
+    while node:
+        n += 1
+        node = node.next
+    k = k % n
+    return _rotate_right(head, k)
+
+
+
+
+
+for ex_list, k in [(range(1, 6), 2), (range(3), 4)]:
+    ex_head = build_linked_list(ex_list)
+    traverse_list(ex_head)
+    print()
+    traverse_list(rotate_right(ex_head, k))
+    print()
+
+
