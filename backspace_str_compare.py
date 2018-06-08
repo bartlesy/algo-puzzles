000eebb02c4cba7c2420bb57441215e472aaadd9
Stanley Bartlett <stanleybartlett@northwesternmutual.com>
2018-06-08 10:15:48 -0400

    backspace str compare naive solution O(N) time and space - should be able to use ptrs to get to O(1) space

---
+++ backspace_str_compare.py
@@ -0,0 +1,32 @@
+from collections import deque
+def get_final_str(in_str):
+    out_str = []
+    for i, c in enumerate(in_str):
+        if c == '#':
+            if out_str:
+                out_str.pop()
+        else:
+            out_str.append(c)
+    return ''.join(out_str)
+
+
+class Solution:
+    def backspaceCompare(self, S, T):
+        """
+        :type S: str
+        :type T: str
+        :rtype: bool
+        """
+        return get_final_str(S) == get_final_str(T)
+
+
+if __name__ == '__main__':
+    test_cases = [
+        ({"S": "a##c", "T": "#a#c"}, True),
+        ({"S": "a#c", "T": "b"}, False)
+    ]
+
+    sln = Solution()
+
+    for case, res in test_cases:
+        print(sln.backspaceCompare(**case), res)
