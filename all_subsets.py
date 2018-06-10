f1b3bb891031d6557c6ec60861a8f08ae0d90758
Stanley Bartlett <stanbartlett@gmail.com>
2018-06-10 11:08:52 -0400

    all subsets

---
+++ all_subsets.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+def all_subsets(nums):
+    """
+    2^N combinations
+    trick is to realize for each number there is a choice w/ 2 options:
+        1. include in subnset
+        2. exclude in subset
+    so, iterate over input and create a copy including this number
+    """
+    paths = [[]]
+    for x in nums:
+        # using i in range pattern to keep from modifying array as
+        # we're iterating over it - causing an infinite loop
+        for i in range(len(paths)):
+            paths.append(paths[i][:] + [x])
+    return paths
+
+if __name__ == '__main__':
+    print(all_subsets([1, 2]))
