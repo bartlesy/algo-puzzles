51f04c0f37ba621529391f025290069b9ba3c95c
Stanley Bartlett <stanleybartlett@northwesternmutual.com>
2018-06-21 15:07:15 -0400

    data structures

---
+++ data_structures/my_queue.py
@@ -0,0 +1,71 @@
+from queue import Queue
+
+class ListNode(object):
+
+    def __init__(self, val, next_node=None):
+        self.val = val
+        self.next = next_node
+        return
+
+
+class MyQueue(object):
+
+    def __init__(self, vals=[]):
+        if not vals:
+            self.head = None
+            self.tail = None
+        else:
+            vals = [ListNode(x) for x in vals]
+            self.head = vals[0]
+            for n1, n2 in zip(vals, vals[1:]):
+                n1.next = n2
+            self.tail = n2 if n2 else self.head
+        return
+
+    def put(self, val):
+        if not self.head:
+            self.head = ListNode(val)
+            self.tail = self.head
+        else:
+            self.tail.next = ListNode(val)
+            self.tail = self.tail.next
+        return
+
+    def pop(self):
+        popped = self.head
+        self.head = popped.next
+        popped.next = None
+        return popped.val
+
+    def __repr__(self):
+        node = self.head
+        out = []
+        while node:
+            out.append(node.val)
+            node = node.next
+        return str(out)
+
+
+if __name__ == '__main__':
+
+    mq = MyQueue()
+    q = Queue()
+    for x in range(10):
+        mq.put(x)
+        q.put(x)
+    print(mq)
+    print(q)
+    print(mq.pop())
+    print(q.get())
+    mq.put(420)
+    q.put(420)
+    print(mq)
+    print(str(q))
+    print(mq.pop())
+    print(mq.pop())
+    print(mq.pop())
+    print(q.get())
+    print(q.get())
+    print(q.get())
+    print(mq)
+    print(str(q))
