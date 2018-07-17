497a05b40d3b0360d99b619498e90d5ff7641df7
Stanley Bartlett <stanleybartlett@northwesternmutual.com>
2018-07-17 17:08:05 -0400

    two sum iv input is a bst

---
+++ two_sum_iv.py
@@ -0,0 +1,23 @@
+class Solution:
+    def findTarget(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: bool
+        """
+        tar_nums = set()
+
+        def traverse(root, k):
+            if not root:
+                return False
+            rem = k - root.val
+            if root.val in tar_nums:
+                return True
+            tar_nums.add(rem)
+            l_res = traverse(root.left, k)
+            r_res = traverse(root.right, k)
+            return l_res or r_res
+        return traverse(root, k)
+
+
+
