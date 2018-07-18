13aedf51b4dcdd0d620f7c4210ba5c90d8d25808
Stanley Bartlett <stanleybartlett@northwesternmutual.com>
2018-07-18 10:06:00 -0400

    advantage shuffle is correct but too slow

---
+++ advantage_shuffle.py
@@ -0,0 +1,58 @@
+"""
+Given two arrays A and B of equal size, the advantage of A with respect to B is the number of indices i for which A[i] > B[i].
+
+Return any permutation of A that maximizes its advantage with respect to B.
+
+
+
+Example 1:
+
+Input: A = [2,7,11,15], B = [1,10,4,11]
+Output: [2,11,7,15]
+Example 2:
+
+Input: A = [12,24,8,32], B = [13,25,32,11]
+Output: [24,32,8,12]
+"""
+from collections import defaultdict, Counter
+
+class Solution:
+    def get_next_ele(self, idx_lu, target):
+        min_advs = list(filter(lambda x: x > target, idx_lu))
+        if min_advs:
+            return min(min_advs)
+        return min(idx_lu)
+
+    def update_ele(self, idx_lu, ele):
+        idx_lu[ele] -= 1
+        if not idx_lu[ele]:
+            del idx_lu[ele]
+        return
+
+    def advantageCount(self, A, B):
+        """
+        :type A: List[int]
+        :type B: List[int]
+        :rtype: List[int]
+        """
+        idx_lu = Counter(A)
+
+        out = []
+        for i, b in enumerate(B):
+            next_ele = self.get_next_ele(idx_lu, b)
+            self.update_ele(idx_lu, next_ele)
+            out.append(next_ele)
+        return out
+
+
+if __name__ == '__main__':
+    sln = Solution()
+    A = [2,7,11,15]
+    B = [1,10,4,11]
+    print(sln.advantageCount(A, B))
+    print("should be", [2, 11, 7, 15])
+    A = [2,0,4,1,2]
+    B = [1,3,0,0,2]
+    print(sln.advantageCount(A, B))
+    print("should be", [2,0,2,1,4])
+
---
+++ car_fleet.py
@@ -0,0 +1,98 @@
+# N cars are going to the same destination along a one lane road.  The destination is target miles away.
+#
+# Each car i has a constant speed speed[i] (in miles per hour), and initial position position[i] miles towards the target along the road.
+#
+# A car can never pass another car ahead of it, but it can catch up to it, and drive bumper to bumper at the same speed.
+#
+# The distance between these two cars is ignored - they are assumed to have the same position.
+#
+# A car fleet is some non-empty set of cars driving at the same position and same speed.  Note that a single car is also a car fleet.
+#
+# If a car catches up to a car fleet right at the destination point, it will still be considered as one car fleet.
+#
+#
+# How many car fleets will arrive at the destination?
+#
+#
+#
+# Example 1:
+#
+# Input: target = 12, position = [10,8,0,5,3], speed = [2,4,1,1,3]
+# Output: 3
+# Explanation:
+# The cars starting at 10 and 8 become a fleet, meeting each other at 12.
+# The car starting at 0 doesn't catch up to any other car, so it is a fleet by itself.
+# The cars starting at 5 and 3 become a fleet, meeting each other at 6.
+# Note that no other cars meet these fleets before the destination, so the answer is 3.
+#
+# Note:
+#
+# 0 <= N <= 10 ^ 4
+# 0 < target <= 10 ^ 6
+# 0 < speed[i] <= 10 ^ 6
+# 0 <= position[i] < target
+# All initial positions are different.
+
+class Solution:
+    def carFleet(self, target, position, speed, n_fleets=0):
+        """
+        :type target: int
+        :type position: List[int]
+        :type speed: List[int]
+        :rtype: int
+        """
+        if not position:
+            return 0
+        position, speed = [list(x) for x in zip(*sorted(zip(position, speed), key=lambda x: x[0]))]
+
+        def _car_fleet(target, position, speed, n_fleets):
+            if not position:
+                return n_fleets
+            if len(position) == 1:
+                return n_fleets + 1
+            fleet_add = 0
+            for i in range(len(position) - 1, -1, -1):
+                pos = position[i]
+                spd = speed[i]
+                if i < (len(position) - 1):
+                    position[i] = min([target, pos + spd, position[i + 1]])
+                else:
+                    position[i] = min([target, pos + spd])
+                if position[i] == target and not fleet_add:
+                    fleet_add = 1
+
+
+            while position:
+                if position[-1] < target:
+                    break
+                position.pop()
+                speed.pop()
+
+            return _car_fleet(target, position, speed, n_fleets + fleet_add)
+        return _car_fleet(target, position, speed, n_fleets)
+
+class Solution(object):
+    def carFleet(self, target, position, speed):
+        cars = sorted(zip(position, speed))
+        times = [float(target - p) / s for p, s in cars]
+        ans = 0
+        print(times)
+        while len(times) > 1:
+            lead = times.pop()
+            if lead < times[-1]:
+                ans += 1  # if lead arrives sooner, it can't be caught
+            else:
+                times[-1] = lead # else, fleet arrives at later time 'lead'
+
+        return ans + bool(times) # remaining car is fleet (if it exists)
+
+if __name__ == '__main__':
+    sln = Solution()
+    print(sln.carFleet(target=12, position=[10,8,0,5,3], speed=[2,4,1,1,3]))
+    print(sln.carFleet(
+        target=69,
+        position=[2, 1, 0, 4, 1],
+        speed=[6, 9, 4, 2, 1]
+    ))
+    print(sln.carFleet(10, [8,3,7,4,6,5], [4,4,4,4,4,4]))
+
