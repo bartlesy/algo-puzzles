ec1013478764c1ee4ea697b4e09c906709108378
Stanley Bartlett <stanleybartlett@northwesternmutual.com>
2018-08-02 08:58:15 -0400

    max depth of n-ary tree

---
+++ max_depth_n_ary_tree.py
@@ -0,0 +1,22 @@
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val, children):
+        self.val = val
+        self.children = children
+"""
+
+
+class Solution(object):
+    def maxDepth(self, root, depth=1):
+        """
+        :type root: Node
+        :rtype: int
+        """
+        if not root:
+            return 0
+        if not root.children:
+            return depth
+        return max([self.maxDepth(child, depth + 1) for child in root.children])
+
+
