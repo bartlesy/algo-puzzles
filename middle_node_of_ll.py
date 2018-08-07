7ae42d5673e23693c03da7d70f146d60df986eec
Stanley Bartlett <stanleybartlett@northwesternmutual.com>
2018-08-07 14:03:58 -0400

    middle node

---
+++ middle_node_of_ll.py
@@ -0,0 +1,26 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def middleNode(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        i = 0
+        middle = head
+        middle_i = 0
+        node = head
+        while node:
+            node = node.next
+            i += 1
+            if middle_i < i // 2:
+                middle_i += 1
+                middle = middle.next
+        return middle
+
+
+
