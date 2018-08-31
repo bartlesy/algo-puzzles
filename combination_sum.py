f467e14bfdc18aa282e87bcd613b79ae8b5202eb
Stanley Bartlett <stanbartlett@gmail.com>
2018-08-31 11:48:57 -0400

    heap implementation

--- combination_sum.py
+++ combination_sum.py
@@ -34,9 +34,21 @@ def combo_sum(num_list, target):
     return out_list
 
 
+def count_num_combinations_without_reuse(num_list, target):
+    num_list = [0] + num_list
+    T = [[0 for _ in range(target + 1)] for __ in num_list]
+    T[0][0] = 1
+    for i in range(1, len(num_list)):
+        for j in range(target + 1):
+            if num_list[i] <= j:
+                T[i][j] = T[i - 1][j] + T[i - 1][j - num_list[i]]
+            else:
+                T[i][j] = T[i - 1][j]
+    return T[-1][-1]
+
+
 if __name__ == '__main__':
     print(combination_sum(list(range(1, 10)), 5))
     print(combo_sum(list(range(1, 10)), 5))
-
-
-
+    print(count_num_combinations_without_reuse(list(range(1, 10)), 5))
+    print(combination_sum([1, 2], 8))
---
+++ data_structures/bin_heap.py
@@ -0,0 +1,77 @@
+class BinHeap(object):
+    """min heap implementation"""
+
+    def __init__(self):
+        # zero is unused but useful for integer division later
+        self.heap_list = [0]
+        self.current_size = 0
+
+    def _swap_idx(self, i1, i2):
+        self.heap_list[i1], self.heap_list[i2] = self.heap_list[i2], self.heap_list[i1]
+        return
+
+    def percolate(self, i):
+        if not i // 2:
+            return
+        if self.heap_list[i] < self.heap_list[i // 2]:
+            self._swap_idx(i, i // 2)
+        self.percolate(i // 2)
+        return
+
+    def insert(self, x):
+        self.heap_list.append(x)
+        self.current_size += 1
+        self.percolate(self.current_size)
+        return
+
+    def min_child(self, i):
+        if (i * 2 + 1) > self.current_size:
+            res = i * 2
+        else:
+            if self.heap_list[i * 2] < self.heap_list[i * 2 + 1]:
+                res = i * 2
+            else:
+                res = i * 2 + 1
+        return res
+
+    def sink(self, i):
+        if (i * 2) <= self.current_size:
+            mc = self.min_child(i)
+            if self.heap_list[i] > self.heap_list[mc]:
+                self._swap_idx(i, mc)
+            self.sink(mc)
+        return
+
+    def del_min(self):
+        # note - min is at 1 because of the placeholder 0
+        ret_val = self.heap_list[1]
+        self.heap_list[1] = self.heap_list[-1]
+        self.current_size -= 1
+        self.heap_list.pop()
+        self.sink(1)
+        return ret_val
+
+    def build_heap(self, in_list):
+        i = len(in_list) // 2
+        self.current_size = len(in_list)
+        self.heap_list = [0] + in_list
+        while i > 0:
+            self.sink(i)
+            i -= 1
+        return
+
+
+if __name__ == '__main__':
+    bh = BinHeap()
+    bh.build_heap([9,5,6,2,3])
+    print(bh.heap_list)
+    bh.insert(1)
+    print(bh.heap_list)
+    bh.insert(2)
+    bh.insert(2)
+    bh.insert(2)
+    print(bh.heap_list)
+
+    while len(bh.heap_list) > 1:
+        print(bh.del_min())
+
